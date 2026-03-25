c0fe6eb7b5e1a7fecab2605eedab20140ead3798
cassoybnr <casmirballesteros@gmail.com>
2026-03-25 22:07:12 +0800

    Milestone 2: Complete

--- studybuddy/app/src/main/java/com/example/studybuddy/SplashActivity.java
+++ studybuddy/app/src/main/java/com/example/studybuddy/SplashActivity.java
@@ -1,24 +1,25 @@
-package com.example.studybuddy;
+package com.example.studybuddy; // Ensure this matches your package name
 
+import android.content.Intent;
 import android.os.Bundle;
-
-import androidx.activity.EdgeToEdge;
+import android.view.View;
+import android.button.Button;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
 
 public class SplashActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
         setContentView(R.layout.activity_splash);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
+
+        Button startBtn = findViewById(R.id.btn_start);
+        startBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(SplashActivity.this, LoginActivity.class);
+                startActivity(intent);
+            }
         });
     }
 }
